d67502ca206aec98c74899dafbe7d189f408a4e4
kapibara001 <trostikshop@gmail.com>
2025-12-14 12:50:19 +0500

    Data parser

--- Project/earthquakes/src/main/java/project/Data_parser.java
+++ Project/earthquakes/src/main/java/project/Data_parser.java
@@ -1,17 +1,38 @@
 package project;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 
 public class Data_parser {
-    private String line;
-    private int year;
+    private Path path;
 
-    public Data_parser(String line, int year) {
-        this.line = line;
-        this.year = year;
+    public Data_parser() {
+        this.path = Path.of("Землетрясения.csv").toAbsolutePath();
     }
 
-    public void print() {
-        System.out.println(line + " " + year);
+    public CSVReader createCSVReader() throws FileNotFoundException {
+        FileReader fileReader = new FileReader(this.path.toString());
+        return new CSVReader(fileReader);
     }
+
+    public List<String[]> readAllData() throws IOException, CsvException {
+        try (CSVReader csvReader = createCSVReader()) {
+            return csvReader.readAll();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, CsvException {
+        Data_parser dp = new Data_parser();
+
+        List<String[]> allData = dp.readAllData();
+
+        System.out.println(allData.get(1)[0]);
+    };
 }
--- Project/earthquakes/src/main/java/project/Main.java
+++ Project/earthquakes/src/main/java/project/Main.java
@@ -1,10 +1,9 @@
 package project;
 
+import com.opencsv.CSVReader;
+
 public class Main {
     public static void main(String[] args) {
-        Data_parser dp = new Data_parser("Sample line from CSV", 2025);
-        dp.print();
-        
 
     }
 }
